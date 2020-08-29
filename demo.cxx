857656b9ea31b1c8982a0b6fe9de2e321bbd126f
ahsanimtiaz53 <47733497+ahsanimtiaz53@users.noreply.github.com>
2020-08-28 20:37:33 -0500

    Add files via upload

---
+++ demo.cxx
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main(int arcg, char* argv[])
+{
+	int age = 0;
+	
+	cout << "How old are you?" << endl;
+	cin >> age;
+	cout << "You are " << age << " years old" << endl;
+	
+	return 0;
+}
